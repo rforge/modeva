225e68ac5629e9350778aaea8445df933f3d309f
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-07 09:03:12 +0000

    new format for input data
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/OA.R
+++ pkg/R/OA.R
@@ -1,17 +1,34 @@
 OA <-
-function(response, predictors) {
-  # version 1.1 (20 May 2013)
-  # response: a vector or column with your response presence-absence variable
-  # predictors: the corresponding variables whose values you want to circumscribe to positive (presence) values of the response; must be in the same order and of the same length as 'response'
-  predictors.presence <- predictors[response > 0, ]
+function(data, sp.cols, var.cols) {
+  # version 2.0 (29 Apr 2014)
+
+  if (length(sp.cols) > 1) stop ("Sorry, OA is currently implemented for only one response variable at a time, so 'sp.cols' must indicate only one column")
+
+  input.data <- data
+  
+  #data <- data.frame(data, 1:nrow(data))
+  #data.noNA <- na.omit(data[ , c(sp.cols, var.cols, ncol(data))])
+  #noNA <- data.noNA[ , ncol(data.noNA)]
+  #data <- data[data[ , ncol(data)] %in% noNA, ]
+  
+  na.rm = TRUE
+  if(na.rm) {
+    mod.data <- data[ , c(sp.cols, var.cols)]
+    data <- data[complete.cases(mod.data), ]
+  }
+  
+  predictors <- data[ , var.cols]
   nvar <- ncol(predictors)
+  
+  response <- data[ , sp.cols]
+  predictors.presence <- predictors[response > 0, ]
   var.presmin <- var.presmax <- vector("numeric", nvar)
   predictors.overlap <- matrix(data = NA, nrow = nrow(predictors), ncol = nvar)
   for (v in 1:nvar) {
-    var.presmin[v] <- min(predictors.presence[ ,v])
-    var.presmax[v] <- max(predictors.presence[ ,v])
-    predictors.overlap[ ,v] <- ifelse((predictors[ ,v] >= var.presmin[v] & predictors[ ,v] <= var.presmax[v]), 1, 0)
+    var.presmin[v] <- min(predictors.presence[ , v])
+    var.presmax[v] <- max(predictors.presence[ , v])
+    predictors.overlap[ , v] <- ifelse((predictors[ , v] >= var.presmin[v] & predictors[ , v] <= var.presmax[v]), 1, 0)
   }  # end for v
-  overlap <- as.integer(rowSums(predictors.overlap) == nvar)
-  return(overlap)
+  overlap.noNA <- as.integer(rowSums(predictors.overlap) == nvar)
+  return(overlap.noNA)
 }

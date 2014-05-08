a60f443ba478b9a52d25aeb2324bc1271fb7c252
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-08 17:26:27 +0000

    added integerCols function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/integerCols.R
+++ pkg/R/integerCols.R
@@ -2,12 +2,12 @@ integerCols <- function(data) {
   is.wholenumber <- function(x, tol = .Machine$double.eps ^ 0.5) {
     abs(x - round(x)) < tol
   }  # from ?is.integer examples
+  all.cols <- 1:ncol(data)
   integer.cols <- rep(0, ncol(data))
-  for (i in 1:ncol(data)) {
+  for (i in all.cols) {
     x <- na.omit(data[ , i])
     if(!is.numeric(x)) next
     if(min(is.wholenumber(x) == 1)) integer.cols[i] <- 1
   }
-  all.cols <- 1:ncol(data)
   data <- multConvert(data, conversion = as.integer, cols = all.cols[integer.cols == 1])
 }

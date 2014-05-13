2c06925d551201b2b83e1c6c4397b49a8e0d00e2
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-13 14:04:05 +0000

    changed data input format
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/FDR.R
+++ pkg/R/FDR.R
@@ -1,19 +1,14 @@
 FDR <-
-function(response, predictors, model.type, pvalues = NULL, 
-                family = "binomial", correction = "fdr", q = 0.05) {
+function(data = NULL, sp.cols = NULL, var.cols = NULL, pvalues = NULL, model.type, 
+         family = "binomial", correction = "fdr", q = 0.05) {
   
-  # version 1.9 (3 Sep 2013)
-  # response: a vector or column with your response variable
-  # predictors: the vectors or columns of your predictor variables
-  # model.type: either "LM" (linear model, for continuous variables) or "GLM" (generalized linear models, for binary or other variables for which these models are more appropriate)
-  # pvalues: optionally, INSTEAD of response and preditors, a table with the names of the predictors in the first column and their univariate p-values (obtained elsewhere) in the second column. Dummy example: pvalues <- data.frame(var = letters[1:5], pval = runif(5, 0, 0.4))
-  # family: in the case of GLM, the error distribution and link function (type ?glm or ?family for details); the default is "binomial" (for binary logistic regression)
-  # correction: the correction procedure to apply to the p-values; type p.adjust.methods for available options, and ?p.adjust for more information
-  # q: the threshold value of FDR-corrected significance above which to reject variables
-  
-  if (missing(model.type)) stop ("'model.type' is missing")
+  if (length(sp.cols) > 1) stop ("Sorry, FDR is currently implemented for only one response variable at a time, so 'sp.cols' must indicate only one column")
+  if (missing(model.type)) stop ("'model.type' is missing; specify either 'LM' or 'GLM'")
   if (!(correction %in% p.adjust.methods)) stop("Invalid correction method. 
 Type 'p.adjust.methods' for available options.")
+  
+  response <- data[ , sp.cols]
+  predictors <- data[ , var.cols]
   
   if (!is.null(pvalues)) {
     coeffs <- FALSE
@@ -23,15 +18,8 @@ Type 'p.adjust.methods' for available options.")
   
   else {
     coeffs <- TRUE
-    if (NA %in% response | NA %in% predictors) 
-      stop("Please remove (rows with) missing values from your data; 
-           note that 'response' and 'predictors' must have the same number 
-           of observations and in the same order.")
     if (is.null(ncol(predictors))) 
       stop("You need more than one predictor to calculate the FDR.")
-    if (!is.vector(response) | length(response) != nrow(predictors)) 
-      stop("'response' must be a vector and have the same length as the number 
-           of rows in 'predictors'.")
     
     p.bivar <- coef.bivar <- vector("numeric", length = ncol(predictors))
     
@@ -51,8 +39,7 @@ Type 'p.adjust.methods' for available options.")
       else stop("'model.type' must be either 'LM' or 'GLM'")
     } # end for i in p.bivar
     
-    if (is.na(p.bivar[i])) message("A p-value could not be calculated for the ", 
-                                   i, "th column in 'predictors'.", sep = "")
+    if (is.na(p.bivar[i])) message("A p-value could not be calculated for var.col number", i)
     
   }  # end else
   
@@ -69,11 +56,11 @@ Type 'p.adjust.methods' for available options.")
     results$p.adjusted <- p.adjust(results$p.value, method = correction)
   }  # end if coeffs else
   
-  select <- subset(results, p.adjusted <= q)
   exclude <- subset(results, p.adjusted > q)
+  select <- subset(results, p.adjusted <= q)
   cat("\nBivariate p-values adjusted with '", correction, 
-      "' correction;\n", nrow(select), " variables selected, ", 
-      nrow(exclude), " excluded\n\n", sep = "")
+      "' correction;\n", nrow(exclude), " variables excluded, ", 
+      nrow(select), " selected\n\n", sep = "")
   
-  return(list(select = select, exclude = exclude))
+  return(list(exclude = exclude, select = select))
 }

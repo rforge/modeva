6b94695da1badc6058b39e86d911b3b391590f8a
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-13 15:36:56 +0000

    NA values now allowed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/MESS.R
+++ pkg/R/MESS.R
@@ -1,31 +1,24 @@
 MESS <- function(V, P){   
-  # version 1.1, 7 May 2014
-  
-  if (NA %in% as.matrix(V) | NA %in% as.matrix(P)) stop("NA values are not allowed.")
+  # version 1.2, 13 May 2014
   
   index.V <- 1:nrow(V)
   index.P <- 1:nrow(P)
-  
   n.vars <- ncol(V)
   n.varP <- ncol(P)
-  
   if(n.vars != n.varP) stop("The number of variables in V and P does not match.")
-  
   n.proj <- nrow(P)
-  
   results <- matrix(nrow = n.proj, ncol = n.vars + 1)
   colnames(results) <- c(colnames(P), "TOTAL")
-  #results <- as.data.frame(results)
   
   for (i in 1:n.vars){
-    min.Vi <- min(V[, i])
-    max.Vi <- max(V[, i])
+    min.Vi <- min(V[, i], na.rm = TRUE)
+    max.Vi <- max(V[, i], na.rm = TRUE)
     SIM <- vector("numeric", n.proj)
     for (j in 1:n.proj){
       VV <- V[, i]
       VV[VV < P[j, i]] <- 1
       VV[VV >= P[j, i]] <- 0
-      Fj <- sum(VV) * 100 / length(VV)
+      Fj <- sum(VV, na.rm = TRUE) * 100 / length(VV)
       if (Fj == 0)  SIM[j] <- (P[j, i] - min.Vi) / (max.Vi - min.Vi) * 100
       else if (Fj > 0 && Fj <= 50)  SIM[j] <- 2 * Fj
       else if (Fj > 50 && Fj < 100)  SIM[j] <- 2 * (100 - Fj)

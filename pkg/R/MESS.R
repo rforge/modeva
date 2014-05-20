89f0328878b41a174235c2df238b0d44c1ac0c83
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-20 18:13:48 +0000

    added timer and MoD to MESS function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/MESS.R
+++ pkg/R/MESS.R
@@ -1,20 +1,23 @@
 MESS <- function(V, P){   
-  # version 1.2, 13 May 2014
+  # version 1.3, 20 May 2014
+  
+  start.time <- Sys.time()
   
   index.V <- 1:nrow(V)
   index.P <- 1:nrow(P)
   n.vars <- ncol(V)
   n.varP <- ncol(P)
   if(n.vars != n.varP) stop("The number of variables in V and P does not match.")
-  n.proj <- nrow(P)
-  results <- matrix(nrow = n.proj, ncol = n.vars + 1)
+  nrow.P <- nrow(P)
+  results <- matrix(nrow = nrow.P, ncol = n.vars + 1)
   colnames(results) <- c(colnames(P), "TOTAL")
   
   for (i in 1:n.vars){
+    message("MESSing variable ", i, " of ", n.vars, "...")
     min.Vi <- min(V[, i], na.rm = TRUE)
     max.Vi <- max(V[, i], na.rm = TRUE)
-    SIM <- vector("numeric", n.proj)
-    for (j in 1:n.proj){
+    SIM <- vector("numeric", nrow.P)
+    for (j in 1:nrow.P){
       VV <- V[, i]
       VV[VV < P[j, i]] <- 1
       VV[VV >= P[j, i]] <- 0
@@ -27,8 +30,21 @@ MESS <- function(V, P){
     results[, i] <- SIM
   }
   
-  for (t in 1:n.proj){
-    results[t, i+1] <- min(results[t, 1:i])
+  message("Calculating TOTAL MESS...")
+  for (t in 1:nrow.P){
+    results[t, "TOTAL"] <- min(results[t, 1:n.vars], na.rm = TRUE)
   }
+  
+  message("Calculating MoD...")
+  results <- data.frame(results, MoD = vector("character", nrow(results)))
+  levels(results[ , "MoD"]) <- colnames(P[ , 1:n.vars])
+  for (r in 1:nrow.P) {
+    results[r, "MoD"] <- names(which.min(results[r, 1:n.vars]))
+  }
+  
+  duration <- difftime(start.time, Sys.time())
+  units <- attr(duration, "units")
+  duration <- round(abs(as.numeric(duration)), 1)
+  message("Finished in ", duration, " ", units)
   return(results)
 }

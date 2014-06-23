27206acbe02f09c87401f9bbb35311e09853f638
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-23 10:39:47 +0000

    improved functions FDR and standard01
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/standard01.R
+++ pkg/R/standard01.R
@@ -1,10 +1,17 @@
 standard01 <-
-function(score, direction = "-1+1to01") {
-  # version 1.0 (18 June 2013)
-  # standardizes a value between -1 and 1 to its corresponding value in the 0-1 scale
+function(score, direction = c("-1+1to01", "01to-1+1")) {
+  # version 1.1 (11 June 2014)
+  # standardizes a value between -1 and 1 to its corresponding value in the 0-1 scale, or vice-versa
+  
+  direction <- match.arg(direction, c("-1+1to01", "01to-1+1"))
+  
   if (direction == "-1+1to01") {
-    score <- score + ((1 - score) / 2)
-  }  # end if -1+1to01
-  else stop ("The 'direction' you chose is not implemented; check for misspelling.")
-  return(score)
+    if (score < -1 | score > 1) stop("'score' must be between -1 and 1")
+    std.score <- score + ((1 - score) / 2)
+  } else {
+    if (score < 0 | score > 1) stop("'score' must be between 0 and 1 to standardize in this direction")
+    std.score <- 2 * (score - 0.5)
+  }
+  
+  return(std.score)
 }

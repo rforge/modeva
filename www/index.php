c9ffa5af68e928855ca43e23c4e4382a339c7f5e
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-08-01 18:33:43 +0000

    slight homepage improvements
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -48,13 +48,18 @@ echo $contents; } ?>
 surface (<b>MESS</b>) <b>analysis</b>, and displaying various <b>analytical plots</b>. Includes also a <b>sample data set</b> with species occurrences and predictor variables.</p>
 <br />
 
+
 <h2> Install and load </h2>
+
 <p>To <strong>install</strong> <i>modEvA</i> directly form R-Forge, paste the following command in the R console (when connected to the internet):</p>
 <code>install.packages("modEvA", repos="http://R-Forge.R-project.org")</code><br />
-<p>If the command above fails, producing a message like "<i>package 'modEvA' is not available for your R version</i>", you can download the compressed package source files to your disk (<i>.zip</i> or <i>.tar.gz</i> available <a href="https://r-forge.r-project.org/R/?group_id=1876">here</a>) and then install the package from there (R menu "<i>Packages - Install packages from local zip files</i>", or "<i>Tools - Install packages - Install from: Package Archive File</i>", or "<i>Packages & Data - Package installer, Packages repository - Local binary package</i>", ... depending on your R interface).</p>
+
+<p>If the command above fails, producing a message like "<i>package 'modEvA' is not available for your R version</i>", you can either upgrade R to the latest version <i>or</i> download the compressed package source files to your disk (<i>.zip</i> or <i>.tar.gz</i> available <a href="https://r-forge.r-project.org/R/?group_id=1876">here</a>) and then install the package from there (R menu "<i>Packages - Install packages from local zip files</i>", or "<i>Tools - Install packages - Install from: Package Archive File</i>", or "<i>Packages & Data - Package installer, Packages repository - Local binary package</i>", ... depending on your R interface).</p>
+
 <p>You only need to install the package once, but then every time you re-open R you need to <strong>load</strong> it by typing:</p>
 <code>library(modEvA)</code><br />
-<p>You can then check out the package <strong>help</strong> files and try out some of the provided <strong>examples</strong>:</p>
+
+<p>You can then check out the package <strong>help</strong> files and try some of the provided <strong>examples</strong>:</p>
 <code>help("modEvA")</code><br /><br />
 
 
@@ -62,9 +67,9 @@ surface (<b>MESS</b>) <b>analysis</b>, and displaying various <b>analytical plot
 
 <h3> Citation of the <i>modEvA</i> package:</h3>
 
-<p>Barbosa A.M., Brown J.A., Jim&eacute;nez-Valverde A. & Real R. (2014) modEvA: Model Evaluation and Analysis. R package, version 0.3</p>
+<p>Barbosa A.M., Brown J.A., Jim&eacute;nez-Valverde A. & Real R. (2014) modEvA: Model Evaluation and Analysis. R package, version 0.6</p>
 
-<p><small><i>NOTE: To get the citation for the version currently installed on your system, load the <i>modEvA</i> package in R and then type </i><b><code></small>citation(package="modEvA")</code></b></p>
+<p><small><i>NOTE: To get the citation for the version currently installed on your system, load the <i>modEvA</i> package in R and then type </i><strong><code></small>citation(package="modEvA")</code></strong></p>
 
 <h3> Papers introducing <i>modEvA</i>:</h3>
 
@@ -76,7 +81,7 @@ surface (<b>MESS</b>) <b>analysis</b>, and displaying various <b>analytical plot
 
 <h2>Find out more</h2>
 
-<p> Click <a href="http://modtools.wordpress.com/packages/modeva/">here</a> for <b>further info</b> on the package, including some <b>user guidelines</b>. </p>
+<p> Click <a href="http://modtools.wordpress.com/packages/modeva/">here</a> for <b>further info</b> on the package and its origins. </p>
 <p> The R-Forge project summary page you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/">here</a>. </p>
 
  </font> 

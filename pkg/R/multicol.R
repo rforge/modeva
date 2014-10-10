9e8af3403652b370d59e1fa2edb9b2cc28cdd7d4
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-10 17:55:49 +0000

    corrected and simplified function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@56 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/multicol.R
+++ pkg/R/multicol.R
@@ -1,16 +1,16 @@
-multicol <- function(data, var.cols = 1:ncol(data)) {
-  result <- matrix(NA, nrow = length(var.cols), ncol = 3)
-  rownames(result) <- colnames(data[ , var.cols])
+multicol <- function(vars) {
+  result <- matrix(NA, nrow = ncol(vars), ncol = 3)
+  rownames(result) <- colnames(vars)
   colnames(result) <- c("Rsquared", "Tolerance", "VIF")
-  for (v in var.cols) {
-    v.name <- colnames(data)[v]
-    other.v.names <- colnames(data)[var.cols[-v]]
+  for (v in 1:ncol(vars)) {
+    v.name <- colnames(vars)[v]
+    other.v.names <- colnames(vars)[-v]
     mod.formula <- as.formula(paste(v.name, "~", paste(other.v.names, collapse = "+")))
-    mod <- lm(mod.formula, data = data)
+    mod <- lm(mod.formula, data = vars)
     R2 <- summary(mod) $ r.squared
     result[v, "Rsquared"] <- R2
     result[v, "Tolerance"] <- 1 - R2
     result[v, "VIF"] <- 1 / (1 - R2)
-  }; rm(v)
+  }
   return(result)
 }

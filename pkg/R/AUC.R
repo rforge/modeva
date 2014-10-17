aeb8c31056152ee2860aec531d82e80b0576801b
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-17 16:44:48 +0000

    added backup source links and minor code tweaks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/AUC.R
+++ pkg/R/AUC.R
@@ -1,10 +1,8 @@
-AUC <-
-function(obs = NULL, pred = NULL, model = NULL, interval = 0.01, 
-                simplif = FALSE, plot = TRUE, plot.values = TRUE, 
-                xlab = c("False positive rate", "(1-specificity)"), 
-                ylab = c("True positive rate", "(sensitivity)"), 
-                main = "ROC curve", ...) {
-  # version 1.3 (4 Sep 2013)
+AUC <- function(obs = NULL, pred = NULL, model = NULL, interval = 0.01, 
+       simplif = FALSE, plot = TRUE, plot.values = TRUE, plot.preds = FALSE, 
+       grid = FALSE, xlab = c("False positive rate", "(1-specificity)"), 
+       ylab = c("True positive rate", "(sensitivity)"), main = "ROC curve", ...) {
+  # version 1.5 (16 Oct 2014)
   
   if (!is.null(model)) {
     if (!is.null(obs)) message("Argument 'obs' ignored in favour of 'model'.")
@@ -25,43 +23,47 @@ function(obs = NULL, pred = NULL, model = NULL, interval = 0.01,
   n1 <- sum(obs == 1)
   n0 <- sum(obs == 0) 
   
-  # next 3 lines: from Wintle et al 2005 supp mat "roc" function
+  # next 3 lines from Wintle et al 2005 supp mat "roc" function
   xy <- c(pred[obs == 0], pred[obs == 1])
   rnk <- rank(xy)
   AUC <- ((n0 * n1) + ((n0 * (n0 + 1))/2) - sum(rnk[1 : n0])) / (n0 * n1)
   
-  if (simplif) {
-    return(AUC)
-  } else {  # end if simplif
-    
-    N <- length(obs)
-    preval <- prevalence(obs)
-    thresholds <- seq(0, 1, by = interval)
-    Nthresh <- length(thresholds)
-    true.pos <- true.neg <- sens <- spec <- false.pos.rate <- numeric(Nthresh)
-    
-    # next lines and plot based on a script by Regina Bispo (ISPA, Lisbon)
-    # (R course NUFOR-UEvora 2009)
-    for (i in 1 : Nthresh) {
-      true.pos[i] <- sum(obs == 1 & pred >= thresholds[i])
-      true.neg[i] <- sum(obs == 0 & pred < thresholds[i])
-      sens[i] <- true.pos[i] / n1
-      spec[i] <- true.neg[i] / n0
-      false.pos.rate[i] <- 1 - spec[i]
-    }  # end for i
-    
-    thresholds = data.frame(thresholds, true.pos, true.neg, 
-                            sens, spec, false.pos.rate)
-  }  # end if simplif else
+  if (simplif)  return(AUC)
+  
+  N <- length(obs)
+  preval <- prevalence(obs)
+  thresholds <- seq(0, 1, by = interval)
+  Nthresh <- length(thresholds)
+  true.positives <- true.negatives <- sensitivity <- specificity <- false.pos.rate <- n.preds <- prop.preds <- numeric(Nthresh)
+  
+  for (t in 1 : Nthresh) {
+    true.positives[t] <- sum(obs == 1 & pred >= thresholds[t])
+    true.negatives[t] <- sum(obs == 0 & pred < thresholds[t])
+    sensitivity[t] <- true.positives[t] / n1
+    specificity[t] <- true.negatives[t] / n0
+    false.pos.rate[t] <- 1 - specificity[t]
+    n.preds[t] <- sum(round(pred, nchar(Nthresh) - 1) == thresholds[t])
+    prop.preds[t] <- n.preds[t] / length(pred)
+  }; rm(t)
   
   if (plot) {
-    plot(c(1, false.pos.rate, 0), c(1, sens, 0), type = "o", pch = "", xlim = c(0, 1), ylim = c(0, 1), xlab = xlab, ylab = ylab, main = main, ...)
-    abline(0, 1, col = "grey", lty = 2)
+    plot(x = c(0, 1), y = c(0, 1), type = "l", xlab = xlab, ylab = ylab, main = main, col = "grey", ...)  # plots the 0.5 diagonal
+
+    if (grid) abline(h = thresholds, v = thresholds, col = "lightgrey")
+  
+    lines(x = false.pos.rate, y = sensitivity, lwd = 2)  # ROC curve
+  
     if (plot.values) {
       text(1.0, 0.1, adj = 1, substitute(paste(AUC == a), list(a = round(AUC, 3))))
-    }  # end if plot values
+    }  # end if plot.values
+    
+    if (plot.preds) {
+      points(x = false.pos.rate, y = sensitivity, cex = rev(20 * sqrt(prop.preds)), col = "red")  # but why do I have to rev?
+      #points(x = thresholds, y = specificity, cex = sqrt(100 * prop.preds), col = "blue")
+      #points(x = false.pos.rate, y = sensitivity, cex = sqrt(100 * prop.preds), col = "red")  # ROC curve
+      points(x = thresholds, y = rep(0, Nthresh), cex = 20 * sqrt(prop.preds), col = "blue")
+    }  # end if plot.preds
   }  # end if plot
   
-  return (list(thresholds = thresholds, N = N, prevalence = preval, AUC = AUC))
-  
+  return (list(thresholds = data.frame(thresholds, true.positives, true.negatives, sensitivity, specificity, false.pos.rate, n.preds, prop.preds), N = N, prevalence = preval, AUC = AUC, AUCratio = AUC / 0.5))
 }
--- www/index.php
+++ www/index.php
@@ -20,7 +20,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 	<link href="http://<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
   </head>
 
-<body bgcolor=#CCF5EB>
+<body bgcolor=#E2FFE2>
 
 <!-- R-Forge Logo -->
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
@@ -54,9 +54,7 @@ echo $contents; } ?>
 
 <code>install.packages("modEvA", repos="http://R-Forge.R-project.org")</code><br />
 
-<p>This should work if you have the <b>latest version of R</b>; otherwise, it may either fail (producing a message like "<i>package 'modEvA' is not available for your R version</i>") or install an older version of <i>modEvA</i>. To <b>check the <i>modEvA</i> version that you have actually installed</b>, type <big><b><code>citation(package="modEvA")</code></b></big>.
-
-To install the latest <i>modEvA</i> version, you can either upgrade R <i>or</i> download the compressed <i>modEvA</i> package source files to your disk (<i>.zip</i> or <i>.tar.gz</i> available <a href="https://r-forge.r-project.org/R/?group_id=1876">here</a>) and then install the package from there (R menu "<i>Packages - Install packages from local zip files</i>", or "<i>Tools - Install packages - Install from: Package Archive File</i>", or "<i>Packages & Data - Package installer, Packages repository - Local binary package</i>", ... depending on your R interface).</p>
+<p>This should work if you have the <b>latest version of R</b>; otherwise, it may either fail (producing a message like "<i>package 'modEvA' is not available for your R version</i>") or install an older version of <i>modEvA</i>. To <b>check the <i>modEvA</i> version that you have actually installed</b>, type <big><b><code>citation(package="modEvA")</code></b></big>. To install the latest <i>modEvA</i> version, you can either upgrade R <i>or</i> download the compressed <i>modEvA</i> <b>package source files</b> to your disk (<i>.tar.gz</i> for Linux/Mac or <i>.zip</i> for Windows, <b>available <a href="https://r-forge.r-project.org/R/?group_id=1876">here</a> or <a href="https://www.dropbox.com/sh/oac92wu1dbfsol4/AAAKVs4oVBBCUtDkPTnbh11Ga?dl=0">here</a></b>) and then install the package from there (R menu "<i>Packages - Install packages from local zip files</i>", or "<i>Tools - Install packages - Install from: Package Archive File</i>", or "<i>Packages & Data - Package installer, Packages repository - Local binary package</i>", ... depending on your R interface).</p>
 
 <p>You only need to install (each version of) the package once, but then every time you re-open R you need to <strong>load</strong> the package by typing:</p>
 <code>library(modEvA)</code><br />

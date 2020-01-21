cb3f3102a5436960a18af8be224db3fef7dad40b
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-01-21 17:03:55 +0000

    updated number of version on CRAN
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@128 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -47,7 +47,7 @@ echo $contents; } ?>
 <p> It includes functions for <b>variation partitioning</b>; calculating several measures of model <b>discrimination</b>, <b>classification</b>, <b>explanatory power</b>, and <b>calibration</b>; <b>optimizing prediction thresholds</b> based on a number of criteria; performing multivariate environmental similarity surface (<b>MESS</b>) <b>analysis</b>; and displaying various <b>analytical plots</b>. It includes also a <b>sample data set</b> with some species distribution models.</p>
 
 <font color="green">
-<h3> Package <i>modEvA</i> (v1.3.2) is on <a href="https://CRAN.R-project.org/package=modEvA">CRAN</a>!</h3>
+<h3> Package <i>modEvA</i> (v2.0) is on <a href="https://CRAN.R-project.org/package=modEvA">CRAN</a>!</h3>
 </font>
 
 <h2> Install and load </h2>
